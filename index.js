a1a862909258f57c5e22ea87a40bc9e06116c28d
lynajiang <77769764+lynajiang@users.noreply.github.com>
2023-01-19 00:35:48 -0800

    Trying to fix inverted mouse.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let mouseCursor = document.querySelector(".cursor");
 let navlinks = document.querySelectorAll('.nav-links li');
+let fancy = document.querySelector(".fancy");
 
 window.addEventListener("mousemove", cursor);
-
+document.body.onmousemove = function(e) {
+    document.documentElement.style.setProperty('--x',(e.clientX)+'px');
+    document.documentElement.style.setProperty('--y',(e.clientY)+'px');
+  }
+  
 function cursor(e) {
     mouseCursor.style.top = e.pageY + "px";
     mouseCursor.style.left = e.pageX + "px";
@@ -17,6 +22,15 @@ navlinks.forEach(link => {
         mouseCursor.classList.add('link-grow');
         link.classList.add("hovered-link");
     });
+});
+
+fancy.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('fancy-grow');
+    fancy.classList.remove("hovered-fancy");
+});
+fancy.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('fancy-grow');
+    fancy.classList.add("hovered-fancy");
 });
 
 const text = document.querySelector(".fancy");
@@ -44,3 +58,38 @@ function complete() {
     clearInterval(timer);
     timer = null;
 }
+
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: '0%',
+        end: '70%',
+        scrub: 1,
+    },
+});
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: '0%',
+        end: '80%',
+        scrub: 1,
+    },
+});
+
+let t3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.home',
+        start: '0%',
+        end: '250%',
+        scrub: 1,
+        pin: true,
+        pinSpacing: false,
+    },
+});
+
+tl.fromTo('.logo', {y:0}, {y: -400})
+tl2.fromTo('.fancy', {scale:6}, {scale:1, top:"2rem", left:"3rem", x:"50%", y:"50%"})
+tl3.to
+
